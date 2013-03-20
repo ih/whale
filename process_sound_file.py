a390a0890460f0984a7c805cb41f19522e2b93cd
ih <irvin.hwang@gmail.com>
2013-03-19 22:37:29 -0700

    script pickles a numpy array of converted sound files

--- process_sound_file.py
+++ process_sound_file.py
@@ -1,19 +1,34 @@
 import numpy as np
+import os
+import pickle
 from scikits.audiolab import Sndfile
-
+import pdb
+#TODO(irvin) make the directory of sound files a parameter to this script
 SOUND_DIRECTORY = 'small_data_sample/right_whale'
 
-test_file = '%s/train12.aiff' % SOUND_DIRECTORY
 
-f = Sndfile(test_file, 'r')
+def convert_aiff_to_array(file_name):
+    f = Sndfile(file_name, 'r')
 
-# Sndfile instances can be queried for the audio file meta-data
-fs = f.samplerate
-nc = f.channels
-enc = f.encoding
+    # Sndfile instances can be queried for the audio file meta-data
+    fs = f.samplerate
+    nc = f.channels
+    enc = f.encoding
 
-# Reading is straightfoward
-data = f.read_frames(1000)
+    # Reading is straightfoward
+    data = f.read_frames(1000)
 
-# This reads the next 1000 frames, e.g. from 1000 to 2000, but as single precision
-data_float = f.read_frames(1000, dtype=np.float32)
+    # This reads the next 1000 frames, e.g. from 1000 to 2000, but as single precision
+    data_float = f.read_frames(1000, dtype=np.float32)
+    return data_float
+
+
+whale_data = np.array([])
+all_whale_sounds = [convert_aiff_to_array('%s/%s' % (SOUND_DIRECTORY, sound_file_name)) for
+                    sound_file_name in os.listdir(SOUND_DIRECTORY)]
+
+whale_data = np.array(all_whale_sounds)
+
+output = open('whale_data.pkl', 'wb')
+
+pickle.dump(whale_data, output)
